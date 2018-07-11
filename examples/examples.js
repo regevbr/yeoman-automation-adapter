394c7770f4a65ad86848beda3845ff38417c0966
regevbr <regevbr@gmail.com>
2018-07-11 22:02:57 +0300

    update example

--- examples/examples.js
+++ examples/examples.js
@@ -6,16 +6,18 @@ const generatorName = 'chrome-ext:app';
 const fs = require('fs');
 const path = require('path');
 
+const silent = true;
+
 const variants = [
   {
-    folder: 'test',
+    name: 'test',
     answers: {
       'appName': 'test',
       'appVersion': '1.0',
     }
   },
   {
-    folder: 'test2',
+    name: 'test2',
     answers: {
       'appName': 'test2',
       'appVersion': '2.1',
@@ -23,24 +25,28 @@ const variants = [
   }
 ];
 
-const runGenerator = (folder, answers) => {
-  folder = path.join(__dirname, '..', folder);
+const runGenerator = (name, answers) => {
+  const folder = path.join(__dirname, '..', name);
   if (!fs.existsSync(folder)) {
     fs.mkdirSync(folder);
   }
-  const adapter = new AutoAdapter(answers);
+  const adapter = new AutoAdapter(answers, silent);
   const env = yeoman.createEnv([], { cwd: folder }, adapter);
   env.register(require.resolve('generator-chrome-ext'), generatorName);
   return new Promise((resolve, reject) => {
-    console.log('Start generating', folder);
-    env.run(generatorName, (err) => {
-      if (err) {
-        console.log('Error generating', folder, err);
-        reject(err);
-      } else {
-        console.log('Finished generating', folder);
-        resolve();
-      }
+    console.log('Start generating', name);
+    const onError = (err) => {
+      console.log('Error generating', name, err);
+      reject(err);
+    };
+    const generator = env.create(generatorName, { args: [], options: { silent } });
+    if (generator instanceof Error) {
+      return onError(generator);
+    }
+    generator.on('error', onError);
+    generator.run(() => {
+      console.log('Finished generating', name);
+      resolve();
     });
   });
 };
@@ -48,10 +54,15 @@ const runGenerator = (folder, answers) => {
 console.log('Start generating');
 const promises = [];
 variants.forEach(variant => {
-  promises.push(runGenerator(variant.folder, variant.answers));
+  promises.push(runGenerator(variant.name, variant.answers));
 });
 Promise.all(promises)
   .then(() => {
-    console.log('done');
+    console.log('Finished generating');
+    process.exit(0);
+  })
+  .catch(() => {
+    console.log('Error generating');
+    process.exit(1);
   });
 
