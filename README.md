4b9a99edddcddfa9aaa8e9992208c7393f9bcde8
regevbr <regevbr@gmail.com>
2018-07-11 20:12:58 +0300

    add silent feature

--- README.md
+++ README.md
@@ -5,10 +5,25 @@ Used when creating automations to run yeoman without user interaction
 
 Installtion
 ---------
-```javascript
+```shell
 npm install yeoman-automation-adapter
 ```
 
 Usage
+---------
+```javascript
+const yeoman = require('yeoman-environment');
+const { AutoAdapter } = require('yeoman-automation-adapter');
+const answers = {
+  'appName': 'test',
+  'appVersion': '1.0',
+};
+const silent = false;
+const adapter = new AutoAdapter(answers, silent);
+const env = yeoman.createEnv([], {}, adapter);
+env.run(...);
+```
+
+Examples
 ---------
 Please see examples dir for fully automated example
--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 
 const logger = require('yeoman-environment/lib/util/log');
 const inquirer = require('inquirer');
+const events = require('events');
+const _ = require('lodash');
 
-function DummyPrompt(answers, q) {
+const logMethods = [
+  'write',
+  'writeln',
+  'ok',
+  'error',
+  'skip',
+  'force',
+  'create',
+  'invoke',
+  'conflict',
+  'identical',
+  'info',
+  'table'
+];
+
+  function DummyPrompt(answers, q) {
   this.answers = answers;
   this.question = q;
 }
@@ -30,19 +47,26 @@ DummyPrompt.prototype.run = function() {
   return Promise.resolve(answer);
 };
 
-function AutoAdapter(answers) {
+function AutoAdapter(answers, silent) {
   answers = answers || {};
   this.promptModule = inquirer.createPromptModule();
   Object.keys(this.promptModule.prompts).forEach(function(promptName) {
     this.promptModule.registerPrompt(promptName, DummyPrompt.bind(DummyPrompt, answers));
   }, this);
+  if (!silent){
+    this.log = logger();
+  } else {
+    const log = this.log = function(){};
+    _.extend(log, events.EventEmitter.prototype);
+    logMethods.forEach(function (methodName) {
+      log[methodName] = function(){ return log};
+    });
+  }
 }
 
 AutoAdapter.prototype.diff = function() {
   return 'Diff issues, please run manually to resolve';
 };
-
-AutoAdapter.prototype.log = logger();
 
 AutoAdapter.prototype.prompt = function(questions, cb) {
   const promise = this.promptModule(questions);
